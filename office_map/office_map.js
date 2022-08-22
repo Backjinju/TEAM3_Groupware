dd73b069d25822e181106feadaee736e4988764a
Hyun910615 <gusldlaehk@gmail.com>
2022-08-22 11:39:27 +0900

    220822 노현

--- office_map/office_map.js
+++ office_map/office_map.js
@@ -1,13 +1,13 @@
-const dropdown = document.querySelector('.group');
-const dropdwonMenu = document.querySelector('.group_employee');
+const dropdown = document.querySelector('.office_map_group');
+const dropdwonMenu = document.querySelector('.office_map_group_employee');
 
 
 dropdown.addEventListener('click',function(){
     dropdwonMenu.style.display='block';
 })
 
-const dropdown2 = document.querySelector('.management_support');
-const dropdwonMenu2 = document.querySelector('.management_support_team');
+const dropdown2 = document.querySelector('.office_map_management_support');
+const dropdwonMenu2 = document.querySelector('.office_map_management_support_team');
 
 
 dropdown2.addEventListener('click',function(){
